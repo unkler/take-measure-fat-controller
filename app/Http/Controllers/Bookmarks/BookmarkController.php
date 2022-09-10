529610e1db5af651b3dbf4cd09551e778020266f
unkler <poru@add-natural.com>
2022-09-10 19:53:26 +0900

    ブックマーク作成のバリデーションに関して #4 対応

--- app/Http/Controllers/Bookmarks/BookmarkController.php
+++ app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use App\Bookmark\UseCase\ShowBookmarkListPageUseCase;
+use App\Http\Requests\CreateBookmarkRequest;
 
 class BookmarkController extends Controller
 {
@@ -121,21 +122,15 @@ class BookmarkController extends Controller
      * 一緒にデータベースに保存する※ユーザーに入力してもらうのは手間なので
      * URLが存在しないなどの理由で失敗したらバリデーションエラー扱いにする
      *
-     * @param Request $request
+     * @param CreateBookmarkRequest $request
      * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function create(Request $request)
+    public function create(CreateBookmarkRequest $request)
     {
         if (Auth::guest()) {
             // @note ここの処理はユーザープロフィールでも使われている
             return redirect('/login');
         }
-
-        Validator::make($request->all(), [
-            'url' => 'required|string|url',
-            'comment' => 'required|string|min:10|max:1000',
-            'category' => 'required|integer|exists:bookmark_categories,id',
-        ])->validate();
 
         // 下記のサービスでも同様のことが実現できる
         // @see https://www.linkpreview.net/
